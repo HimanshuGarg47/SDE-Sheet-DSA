ed65315e6a9acecf33fa00182c14e0c4df37630c
Himanshu Garg <himan.gargcoder@gmail.com>
2022-07-11 11:53:48 -0700

    fix debugger and reverse pair solved after watching sol

--- Arrays-Part-III/reverse_pair.cpp
+++ Arrays-Part-III/reverse_pair.cpp
@@ -1,76 +1,75 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
-int merging(vector<int> &arr,int low, int mid, int high)
+
+// guide https://takeuforward.org/data-structure/count-reverse-pairs/
+int merging(vector<int> &arr, int low, int mid, int high)
 {
-    int i = low , j = mid, k = 0,n = high-low+1 , count = 0;
-    vector<int> temp(n);
-    
-    while(i < mid && j <= high)
+
+    int j = mid, count_inv = 0;
+    for (int i = low; i < mid; i++)
     {
-        if(arr[i] > 2*arr[j])
+        while (j <= high && arr[i] > 2 * arr[j])
         {
-            temp[k++] = arr[j];
-            count += (mid-i);
             j++;
         }
-        else if(arr[i] > arr[j])
+        count_inv += (j - mid);
+    }
+
+    int i = low;
+    j = mid;
+    int k = 0, n = high - low + 1;
+    vector<int> temp(n);
+
+    while (i < mid && j <= high)
+    {
+        if (arr[i] > arr[j])
         {
-            temp[k++] = arr[j];
-            j++;
+            temp[k++] = arr[j++];
         }
         else
         {
-            temp[k++] = arr[i];
-            i++;
+            temp[k++] = arr[i++];
         }
     }
-    
-    while(i < mid)
+
+    while (i < mid)
     {
-        temp[k++] = arr[i];
-            i++;
+        temp[k++] = arr[i++];
     }
-    
-    while(j <= high)
+
+    while (j <= high)
     {
-        temp[k++] = arr[j];
-            j++;
+        temp[k++] = arr[j++];
     }
-    
-    for(int y = 0;y<n;y++)
+
+    for (int y = 0; y < n; y++)
     {
         arr[low++] = temp[y];
     }
-    return count;
+    return count_inv;
 }
 
-
-
-
-
-
-
-int mergeSort(vector<int> &arr,int low, int high)
+int mergeSort(vector<int> &arr, int low, int high)
 {
     int mid = 0, count = 0;
-    while(low < high)
+    if (low < high)
     {
-        mid = low+(high-low)/2;
-      count +=  mergeSort(arr,low,mid);
-      count +=   mergeSort(arr,mid+1,high);
-     count += merging(arr,low,mid+1,high);
+        mid = low + (high - low) / 2;
+        count += mergeSort(arr, low, mid);
+        count += mergeSort(arr, mid + 1, high);
+        count += merging(arr, low, mid + 1, high);
     }
-    
+
     return count;
 }
-int reversePairs(vector<int> &arr, int n){
-	return mergeSort(arr,0,n-1);
+int reversePairs(vector<int> &arr, int n)
+{
+    return mergeSort(arr, 0, n - 1);
 }
-
 
 int main()
 {
 
     vector<int> arr = {1, 2, 3, 2, 3, 1};
-    cout << reversePairs(arr,6);
+    cout << reversePairs(arr, 6);
 }
